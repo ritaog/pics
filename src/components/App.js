1f04ba7ea287c4bfb701cfac4ba2d17f79db437a
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2021-12-18 23:06:43 -0700

    Moves Api calls in app.js to unsplash.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React from "react";
+import unsplash from "../api/unsplash.js";
+import dotenv from "dotenv";
 import "semantic-ui-css/semantic.min.css";
 import SearchBar from "./SearchBar.js";
 
+dotenv.config({ path: "./config/config.env" });
 class App extends React.Component {
-  onSearchSubmit = (term) => {
-    console.log(term);
+  state = {
+    images: [],
+  };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+
+    this.setState({ images: response.data.results });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onUserSubmit={this.onSearchSubmit} />
+        <div className="ui segment">{this.state.images.length}</div>
       </div>
     );
   }
