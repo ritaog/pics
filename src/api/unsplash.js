83dc019b4e2f6ac4b5b95d3ee4f14ab5fbfcaa63
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2022-01-08 01:09:41 -0700

    removes api key from file

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,7 +4,6 @@ import apiKey from "./config/api_key";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    // Authorization: "Client-ID H_vRmU096P-jS7BluBjg1y2AurdmZIIdEKalTNu4ths",
     Authorization: `Client-ID ${apiKey()}`,
   },
 });
